f36defc812fcfac116f6ec400d5e9d271548986b
LionyxML <rahul.juliato@gmail.com>
2021-05-02 15:41:28 -0300

    Tests with tasks. Created dummy tasks. Globalized list with useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+
+      {
+        id: 1,
+        text: 'Comprar comida para os gatos',
+        day: '5 de Junho às 18:30',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Reunião na escola',
+        day: '10 de Dezembro às 14:00',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Mercado',
+        day: '22 de Dezembro às 17:00',
+        reminder: false,
+      }
+  ])
+
   return (
     <div className="container">
         <Header />
+        <Tasks tasks={tasks} />
     </div>
 
   );
